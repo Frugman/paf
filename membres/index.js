35711dc944ffc21cfcb55f2ebd090fff038a543d
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 23:15:12 +0100

    Suppression membre : testtest (Index update)

--- membres/index.js
+++ membres/index.js
@@ -1,4 +1,3 @@
-import { testtest } from './testtest.js';
 import { mireillecaulier } from './mireillecaulier.js';
 // 1. Importer les fiches des membres ici
 // Attention : le nom entre accolades {} doit être EXACTEMENT le même que dans le fichier du membre
@@ -6,8 +5,7 @@ import { hortensechevalier } from './hortensechevalier.js';
 
 // 2. Ajouter les membres dans la liste finale
 export const lotsOfMembers = [
-    testtest,
-    mireillecaulier,
+        mireillecaulier,
     hortensechevalier,
     // Quand vous aurez une nouvelle membre (ex: Fatou), vous ajouterez :
     // fatouDiop,
