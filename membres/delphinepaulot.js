0441a41b0f1a28e59a3b59972c384e3067a8237d
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 23:41:46 +0100

    Ajout membre : delphinepaulot.js

---
+++ membres/delphinepaulot.js
@@ -0,0 +1,24 @@
+export const delphinepaulot = {
+    id: "delphinepaulot",
+    prenom: "Delphine",
+    nom: "Paulot",
+    nom_code: "Lionne du désert",
+    poste: "Cheffe de projet",
+    role_supp: "",
+    ville: "Treillières",
+    codePostal: "",
+    lat: 47.311114,
+    lng: -1.63115,
+    photo: "https://www.notion.so/image/https%3A%2F%2Fstorage.tally.so%2Fprivate%2FCapture-d-ecran-2025-12-22-a-10.11.15.png%3Fid%3DNMAJWG%26accessToken%3DeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik5NQUpXRyIsImZvcm1JZCI6Ik5wRDREcCIsImlhdCI6MTc2NjM5NDcwMX0.solgx7bIre_ElrqrfWijHHWOhnv4F70gWPRdwgzqYWk%26signature%3Ddb686ae22a4541c48bd7a89f8a60439241a5e1aa9a25b43c5fda55e9faef2a7d?table=block&id=2d1a11f7-eccf-811c-a454-da2adb209afc&spaceId=497a11f7-eccf-81de-9190-0003ae904935&width=1750&userId=24fd872b-594c-81f6-a03a-0002721ef6cb&cache=v2",
+    bio: "Ma vie mon oeuvre",
+    competences: ["Curiosité"],
+    sujets_interet: ["Justice sociale","Interculturalité"],
+    stats: {
+        "bagou": 3,
+        "redac": 4,
+        "terrain": 4,
+        "orga": 4
+    },
+    linkedin: "",
+    email: ""
+};
