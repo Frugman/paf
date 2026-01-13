19b42bd5938de6b227ad28079f9509f1ee2b19eb
Hortense <hchevalier.capnum@gmail.com>
2026-01-13 21:48:38 +0100

    Nouvelle inscription : mireillecaulier.js

---
+++ membres/mireillecaulier.js
@@ -0,0 +1,22 @@
+export const mireillecaulier = {
+    id: "mireillecaulier",
+    prenom: "Mireille",
+    nom: "Caulier",
+    nom_code: "",
+    poste: "",
+    role_supp: "",
+    ville: "Mouilleron-le-Captif",
+    codePostal: "85000",
+    photo: "https://ui-avatars.com/api/?name=Mireille+Caulier&background=random&color=fff&size=256",
+    bio: "",
+    competences: [],
+    sujets_interet: [],
+    stats: {
+        "bagou": 3,
+        "redac": 3,
+        "terrain": 3,
+        "orga": 3
+    },
+    linkedin: "",
+    email: ""
+};
