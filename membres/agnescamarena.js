5458b23b22475bd6e7586c9f9a3c3cbafe9db9e5
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 00:07:37 +0100

    Ajout membre : agnescamarena.js

---
+++ membres/agnescamarena.js
@@ -0,0 +1,23 @@
+export const agnescamarena = {
+    id: "agnescamarena",
+    prenom: "Agnès",
+    nom: "Camarena",
+    nom_code: "Crocodile du nil",
+    poste: "Retraitée et présidente d'association",
+    role_supp: "",
+    ville: "Laval",
+    lat: 48.059373,
+    lng: -0.766849,
+    photo: "https://media.ouest-france.fr/v1/pictures/MjAyNDAzY2FiM2IxNTMyNTVmMjM2NDk4M2RiODIwMmE0N2UxYWI?width=1260&height=708&focuspoint=50%2C25&cropresize=1&client_id=bpeditorial&sign=87ddb1f32effa9c50fe0095a38e7ad1ad930949086cc813e320437a9051bf380",
+    bio: "Ma vie mon oeuvre",
+    competences: ["Contacts","lieux"],
+    sujets_interet: ["Justice sociale","Féminisme"],
+    stats: {
+        "bagou": 5,
+        "redac": 3,
+        "terrain": 4,
+        "orga": 3
+    },
+    linkedin: "",
+    email: ""
+};
