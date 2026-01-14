47e7b5ff71792ba624ac1501fbde5a6421147937
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 08:16:01 +0100

    Ajout membre : agnescamarena.js

--- membres/agnescamarena.js
+++ membres/agnescamarena.js
@@ -8,7 +8,7 @@ export const agnescamarena = {
     ville: "Laval",
     lat: 48.059373,
     lng: -0.766849,
-    photo: "https://media.ouest-france.fr/v1/pictures/MjAyNDAzY2FiM2IxNTMyNTVmMjM2NDk4M2RiODIwMmE0N2UxYWI?width=1260&height=708&focuspoint=50%2C25&cropresize=1&client_id=bpeditorial&sign=87ddb1f32effa9c50fe0095a38e7ad1ad930949086cc813e320437a9051bf380",
+    photo: "https://www.linkedin.com/me?trk=p_mwlite_feed-secondary_nav",
     bio: "Ma vie mon oeuvre",
     competences: ["Contacts","lieux"],
     sujets_interet: ["Justice sociale","Féminisme"],
