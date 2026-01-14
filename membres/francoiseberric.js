173155a5c721c6f427a74063d3ff6a999b43be36
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 07:55:54 +0100

    Ajout membre : francoiseberric.js

---
+++ membres/francoiseberric.js
@@ -0,0 +1,23 @@
+export const francoiseberric = {
+    id: "francoiseberric",
+    prenom: "Françoise",
+    nom: "Berric",
+    nom_code: "Soiz-des-bois",
+    poste: "Enseignante et médiatrice culturelle",
+    role_supp: "Cheffe de file locale",
+    ville: "Thouaré sur Loire",
+    lat: 47.275629,
+    lng: -1.437708,
+    photo: "https://ui-avatars.com/api/?name=Françoise+Berric&background=random&color=fff&size=256",
+    bio: "Après une formation scientifique,  je suis devenue enseignante spécialisée dans les difficultés d'apprentissage. Passionnée de théâtre, je joue depuis 40 ans dans une troupe  et porte aujourd'hui des projets théâtre  avec les institutions . Les valeurs qui me sont chères sont celles d'équité, d'inclusion, de respect, que je défends entre autres par le théâtre et la médiation culturelle.",
+    competences: ["Mise en voix","interprétation","animation théâtrale"],
+    sujets_interet: ["Égalité des chances","inclusion","justice sociale"],
+    stats: {
+        "bagou": 3,
+        "redac": 4,
+        "terrain": 3,
+        "orga": 3
+    },
+    linkedin: "https://www.linkedin.com/in/fran%C3%A7oise-berric-78029b265",
+    email: "francoiseberric@gmail.com"
+};
