23dac46cb669a12bf004e09f5fc5a40f2598527a
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 07:58:35 +0100

    Ajout membre : mireillecaulier.js

--- membres/mireillecaulier.js
+++ membres/mireillecaulier.js
@@ -3,21 +3,20 @@ export const mireillecaulier = {
     prenom: "Mireille",
     nom: "Caulier",
     nom_code: "Aigle de feu",
-    poste: "Retraitée",
+    poste: "Retraitée cadre dirigeant Sécurité Sociale",
     role_supp: "Référente PP85",
     ville: "Mouilleron-le-captif",
-    codePostal: "",
     lat: 46.709533,
     lng: -1.461904,
     photo: "https://media.licdn.com/dms/image/v2/C5603AQGNzVood62LWQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517802702208?e=2147483647&v=beta&t=Ynun2eSHqDve3g2sNN7USyuyItQzXwjd1zEQN0yguNs",
-    bio: "Ma vie mon oeuvre",
-    competences: ["Gestion RH"],
-    sujets_interet: ["Eolien"],
+    bio: "Une vie professionnelle intense portée par deux activités successives : conseil aux agriculteurs puis pilotage  d'organismes de sécurité sociale. Une retraite engagée sur le climatique (groupe de travail ENERCOOP) et le politique (Place Publique).",
+    competences: ["Formation scientifique : rigueur et goût des chiffres. Goût pour les relations humaines :Gestion RH"],
+    sujets_interet: ["Souveraineté énergétique (électricité verte dont l'éolien."],
     stats: {
-        "bagou": 3,
+        "bagou": 4,
         "redac": 3,
         "terrain": 3,
-        "orga": 3
+        "orga": 5
     },
     linkedin: "https://fr.linkedin.com/in/mireille-caulier-b55634110",
     email: "caulierm@sfr.fr"
