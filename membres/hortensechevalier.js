874db5fdd2931beb5ca5ca5ae79dd9477887979d
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 16:44:19 +0100

    Ajout membre : hortensechevalier.js

--- membres/hortensechevalier.js
+++ membres/hortensechevalier.js
@@ -6,18 +6,17 @@ export const hortensechevalier = {
     poste: "Consultante data et innovation",
     role_supp: "Co-référente PP85",
     ville: "La Roche-sur-Yon",
-    codePostal: "85000",
-    lat: 46.670848,
-    lng: -1.412533,
+    lat: 46.687118,
+    lng: -1.447803,
     photo: "https://capnum.io/wp-content/uploads/2024/01/IMG_20210416_120814-1024x1024.jpg",
     bio: "J'accompagne quelques entreprises et des personnes publiques en data et innovation, j'ai aussi un blog sur la data et l'environnement qui me passionne vraiment.",
     competences: ["Stratégie Data","Open Data","Facilitation","Environnement","IA vibe-coding","Webmarketing"],
     sujets_interet: [],
     stats: {
-        "bagou": 4,
-        "redac": 4,
+        "bagou": 3,
+        "redac": 5,
         "terrain": 3,
-        "orga": 5
+        "orga": 4
     },
     linkedin: "https://www.linkedin.com/in/hortensechevalier/",
     email: "consultant.numerique@gmail.com"
