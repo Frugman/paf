bb04960acbb24bdbaa7fc1674cac36f5c0ba8d73
Hortense <hchevalier.capnum@gmail.com>
2026-01-14 17:16:23 +0100

    Ajout membre : marionguerin.js

---
+++ membres/marionguerin.js
@@ -0,0 +1,23 @@
+export const marionguerin = {
+    id: "marionguerin",
+    prenom: "Marion",
+    nom: "GUERIN",
+    nom_code: "évolutionnaire",
+    poste: "Responsable gouvernance partenariat",
+    role_supp: "Cheffe de file Les Sables d'Olonne",
+    ville: "LES SABLES D'OLONNE",
+    lat: 46.524364,
+    lng: -1.765848,
+    photo: "https://ui-avatars.com/api/?name=Marion+GUERIN&background=random&color=fff&size=256",
+    bio: "Engagée auprès de Place Publique depuis les dernières élections européennes, j'ai depuis toujours travaillé pour la \"chose publique\". Je milite pour faire de la politique autrement, pour passer d’une politique de l’ego à un engagement réciproque et responsable.",
+    competences: ["RSE","gouvernance territoriale","développement économique des territoires"],
+    sujets_interet: ["Europe","Autonomie","Agriculture souveraine et durable","droit des femmes","évolution des consciences"],
+    stats: {
+        "bagou": 4,
+        "redac": 4,
+        "terrain": 3,
+        "orga": 2
+    },
+    linkedin: "https://www.linkedin.com/in/marion-guerin-2b170924/",
+    email: "marion-guerin@orange.fr"
+};
