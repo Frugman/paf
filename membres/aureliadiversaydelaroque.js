d556b9157f0d2a84878d5d8442d22fdf267ade6a
Hortense <hchevalier.capnum@gmail.com>
2026-01-20 11:47:40 +0100

    Ajout membre : aureliadiversaydelaroque.js

--- membres/aureliadiversaydelaroque.js
+++ membres/aureliadiversaydelaroque.js
@@ -8,7 +8,7 @@ export const aureliadiversaydelaroque = {
     ville: "Saint Etienne de Montluc 44",
     lat: 47.272362,
     lng: -1.783443,
-    photo: "https://ui-avatars.com/api/?name=Aurélia+DIVERSAY-DELAROQUE&background=random&color=fff&size=256",
+    photo: "https://www.antigone-avocats.com/wp-content/uploads/2021/05/0117_Helene_Charier_Photographe_-_Antigone_-_Web-22.jpg",
     // On remplace les guillemets ET les sauts de ligne (qui cassent le code)
     bio: "46 ans - Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers - Mère de 3 filles 17-13-9 ans - Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc) - Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025. Adhérente au GNSA.",
     competences: ["Prise de parole - organisation de rencontres et échanges","contact facile pour tracter"],
