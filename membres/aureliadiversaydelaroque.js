c1d62298f2f250f50ec83f4b699c97b118f8660c
Hortense <hchevalier.capnum@gmail.com>
2026-01-20 10:11:41 +0100

    Update aureliadiversaydelaroque.js

--- membres/aureliadiversaydelaroque.js
+++ membres/aureliadiversaydelaroque.js
@@ -9,11 +9,7 @@ export const aureliadiversaydelaroque = {
     lat: 47.272362,
     lng: -1.783443,
     photo: "https://ui-avatars.com/api/?name=Aurélia+DIVERSAY-DELAROQUE&background=random&color=fff&size=256",
-    bio: "46 ans
-Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers
-Mère de 3 filles 17-13-9 ans
-Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc)
-Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025. Adhérente au GNSA.",
+    bio: "46 ans - Avocate depuis 2005 au Barreau de Nantes en urbanisme fonction publique environnement côté agents associations particuliers - Mère de 3 filles 17-13-9 ans - Investie sur ma commune au sein de L’amicale laique section art de la rue (jonquilles en fêtes) et section culturelle (festival bouge montluc) - Adhérente PP depuis oct 2023, réferente départementale 02/2024 septembre 2025. Adhérente au GNSA.",
     competences: ["Prise de parole - organisation de rencontres et échanges","contact facile pour tracter"],
     sujets_interet: ["Justice","Environnement","la connerie"],
     stats: {
